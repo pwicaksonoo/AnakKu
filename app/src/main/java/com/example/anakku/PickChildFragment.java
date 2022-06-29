e6385465dd4bce7bd20d0ac197b285c29d0674ad
Florence <florence>
2022-06-29 23:45:56 +0700

    RegisterChildFragment LoginFragment PilihProfilAnakFragment

--- app/src/main/java/com/example/anakku/PickChildFragment.java
+++ app/src/main/java/com/example/anakku/PickChildFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PilihProfilAnakFragment#newInstance} factory method to
+ * Use the {@link PickChildFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PilihProfilAnakFragment extends Fragment {
+public class PickChildFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class PilihProfilAnakFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public PilihProfilAnakFragment() {
+    public PickChildFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class PilihProfilAnakFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PilihProfilAnakFragment.
+     * @return A new instance of fragment PickChildFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static PilihProfilAnakFragment newInstance(String param1, String param2) {
-        PilihProfilAnakFragment fragment = new PilihProfilAnakFragment();
+    public static PickChildFragment newInstance(String param1, String param2) {
+        PickChildFragment fragment = new PickChildFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class PilihProfilAnakFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pilih_profil_anak, container, false);
+        return inflater.inflate(R.layout.fragment_pick_child, container, false);
     }
 }
