6bc76a3ce39fd487cf7ed946af1d7948d72a4d37
Prayogi Wicaksono <cs.prayogiwicaksono@gmail.com>
2022-07-01 09:55:46 +0700

    Child Profile mending pake Fragment atau Activity?

---
+++ app/src/main/java/com/example/anakku/ChildProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.anakku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChildProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_profile);
+    }
+}
