9c0f512bfef4157f02ef3b3905860aa775f555ea
sirzechs753 <sirzechs753@gmail.com>
2022-07-16 00:50:33 +0700

    navigasi semua2

--- app/src/main/java/com/example/anakku/ForumFragment.java
+++ app/src/main/java/com/example/anakku/ForumFragment.java
@@ -4,17 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
-public class ArticleChildViolenceFragment extends Fragment {
-
-    private ImageView articleImageView;
-
+public class ForumFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +18,7 @@ public class ArticleChildViolenceFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_article_child_violence, container, false);
-
-        articleImageView = view.findViewById(R.id.imageViewArticle);
-        articleImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(getView()).navigate(R.id.action_articleChildViolenceFragment_to_forumFragment);
-            }
-        });
+        View view = inflater.inflate(R.layout.fragment_forum, container, false);
 
         return view;
     }
